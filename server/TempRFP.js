98dc311f612a38971b2f49671d854f78e3cc928f
Brian Story <bstory32@gmail.com>
2015-03-02 19:36:17 -0600

    added contactUs Collections

---
+++ server/TempRFP.js
@@ -0,0 +1,25 @@
+if (Rfps.find().count() === 0) {
+  Rfps.insert({
+    name: 'Brian Story',
+    email: 'bstory32@Gmail.com',
+    phone: '7737770000',
+    product: 'Design',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aliquid eveniet reprehenderit! Veritatis consectetur eligendi vitae, quasi est aliquid corrupti praesentium ad, repellat, aliquam eum perspiciatis provident. Molestiae soluta, velit.'
+  });
+  Rfps.insert({
+    name: 'Brian Story',
+    email: 'bstory32@Gmail.com',
+    phone: '7737770000',
+    product: 'Design',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aliquid eveniet reprehenderit! Veritatis consectetur eligendi vitae, quasi est aliquid corrupti praesentium ad, repellat, aliquam eum perspiciatis provident. Molestiae soluta, velit.'
+  });
+  Rfps.insert({
+    name: 'Brian Story',
+    email: 'bstory32@Gmail.com',
+    phone: '7737770000',
+    product: 'Design',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aliquid eveniet reprehenderit! Veritatis consectetur eligendi vitae, quasi est aliquid corrupti praesentium ad, repellat, aliquam eum perspiciatis provident. Molestiae soluta, velit.'
+  });
+
+  
+}
